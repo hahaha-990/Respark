0f05d83d658f6720efea76870f3bb6f4e64e4a40
Jeff Aiman <jeffaiman04@gmail.com>
2026-04-06 14:51:04 +0800

    Deleted system syok sendiri

--- Respark/includes/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function clean($data) {
-    return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
-}
-
-function validateEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-?>
--- Respark/user/view_parking.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../config/db.php";
-
-$stmt = $pdo->query("SELECT * FROM parking WHERE Status='available'");
-$slots = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($slots);
-?>
